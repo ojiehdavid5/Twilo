74d2d9f1efd3299f3cbe93c6d7e5fe48a9b5d5b9
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-02-07 15:11:15 +0100

    changes

---
+++ server.js
@@ -0,0 +1,11 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.calls
+      .create({
+         url: 'http://demo.twilio.com/docs/voice.xml',
+         to: '+123456789',
+         from: '+987654321'
+       })
+      .then(call => console.log(call.sid));
